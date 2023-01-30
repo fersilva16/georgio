a6f73c9a32a6c14018510a4f352578659d2544f2
Fernando Silva <fernandonsilva16@gmail.com>
2023-01-30 20:35:50 -0300

    feat(organize): organize (#4)

--- src/notion/notion.ts
+++ src/notion/notion.ts
@@ -1,6 +1,6 @@
 import { Client } from '@notionhq/client';
 
-import { config } from './config';
+import { config } from '../config';
 
 export const notion = new Client({
   auth: config.NOTION_AUTH,
