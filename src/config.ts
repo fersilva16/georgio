eb6cae9902081089ce8055cb53a57686a767de9b
Fernando Silva <fernandonsilva16@gmail.com>
2023-01-29 22:05:17 -0300

    feat(notion): add integration (#2)

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export const config = {
   DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN as string,
+  NOTION_AUTH: process.env.NOTION_AUTH as string,
 };
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import 'dotenv-safe/config';
-import { client } from './client';
 import { config } from './config';
+import { discord } from './discord';
 
-client.on('ready', () => {
+discord.on('ready', () => {
   // eslint-disable-next-line no-console
   console.log('Georgio is online!');
 });
 
-client.login(config.DISCORD_BOT_TOKEN);
+discord.login(config.DISCORD_BOT_TOKEN);
