f3a11b5ccd53e8a5d5492697e92a1384e7e52d37
Fernando Silva <fernandonsilva16@gmail.com>
2023-01-29 21:17:19 -0300

    feat(discord): add integration (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-// eslint-disable-next-line no-console
-console.log('Hello World');
+import 'dotenv-safe/config';
+import { client } from './client';
+import { config } from './config';
+
+client.on('ready', () => {
+  // eslint-disable-next-line no-console
+  console.log('Georgio is online!');
+});
+
+client.login(config.DISCORD_BOT_TOKEN);
