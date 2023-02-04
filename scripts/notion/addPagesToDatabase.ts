faee1f7dd05ad77f564c1d4407b67b766791f628
Fernando Silva <fernandonsilva16@gmail.com>
2023-02-04 16:50:17 -0300

    chore(scripts): add addPagesToDatabase (#7)

---
+++ scripts/notion/addPagesToDatabase.ts
@@ -0,0 +1,25 @@
+import { notion } from '../../src/notion/notion';
+
+(async () => {
+  const pages = [
+    'Getting real - Basecamp',
+    'Art of Scalability, The: Scalable Web Architecture, Processes, and Organizations for the Modern Enterprise',
+  ];
+
+  for (const page of pages) {
+    await notion.pages.create({
+      parent: {
+        database_id: 'fba3b82ff79e467487fdb118909724f1',
+      },
+      properties: {
+        Name: {
+          type: 'title',
+          title: [{ text: { content: page } }],
+        },
+      },
+    });
+
+    // eslint-disable-next-line no-console
+    console.log(`Page "${page}" created`);
+  }
+})();
